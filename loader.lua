2f92c0fe62d95b5b41194336a51326263953de01
xoodxoodxoodgame-glitch <xoodxoodxoodgame@gmail.com>
2026-02-08 20:55:43 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -114,7 +114,7 @@ end
 -- Module loader function using loadstring
 local function loadModule(moduleName)
     local success, module = pcall(function()
-        return loadstring(game:HttpGet("https://raw.githubusercontent.com/yourusername/roblox_scripts/main/modules/" .. moduleName .. ".lua"))()
+        return loadstring(game:HttpGet("https://raw.githubusercontent.com/xoodxoodxoodgame-glitch/roblox_scripts/main/" .. moduleName .. ".lua"))()
     end)
     
     if success then
